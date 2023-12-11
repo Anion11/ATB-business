332c35d35ac2bd08943d225d7a584db83735dc90
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2023-12-11 18:23:15 +0300

    fix table & fix collateral

--- src/pages/express-credits/express-credits.js
+++ src/pages/express-credits/express-credits.js
@@ -1,7 +1,5 @@
 import uiInput from '../../blocks/_ui/ui-input/ui-input'
-import compareExpressAccordion from '../../blocks/compare-express/compare-express'
 
 document.addEventListener('DOMContentLoaded', function () {
   uiInput()
-  compareExpressAccordion()
 })
